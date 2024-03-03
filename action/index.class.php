add273fcd91f9d2eb7ba1b56130d3beddaee53cd
Your Name <you@example.com>
2024-03-03 13:47:03 +0100

    News will now show multiple pages

--- action/index.class.php
+++ action/index.class.php
@@ -3,6 +3,7 @@
 namespace apexx\modules\news\action;
 
 use apexx\modules\core\EXECUTION_TYPE;
+use apexx\modules\core\Pager;
 
 class Index extends \apexx\modules\core\IAction
 {
@@ -12,53 +13,36 @@ class Index extends \apexx\modules\core\IAction
         $userModule = $this->module()->core()->module("user");
         $user = $userModule->currentUser();
 
-        $statement = $this->prepare(
-            "SELECT 
-                a.id AS ID, 
-                UNIX_TIMESTAMP(`time`) AS `TIME`, 
-                a.title as TITLE, 
-                `text` AS `TEXT`,
-                b.name AS CAT_NAME
-            FROM
-                APEXX_PREFIX_news AS a   
-            LEFT JOIN 
-                APEXX_PREFIX_news_category AS b ON (a.category = b.id)
-                
-                WHERE
-                " . ( $this->param()->getIf("catid") ? "b.id = " . $this->param()->getInt("catid")." AND " : "" ). "
-                " . ($user->hasRight("news", "index", EXECUTION_TYPE::ADMIN) ? "" : "  UNIX_TIMESTAMP(`time`) < ".time()." AND `time` IS NOT NULL AND ") . "
-                1=1
+        $pager = new Pager($this->module()->core(),
+            "a.id AS ID, UNIX_TIMESTAMP(`time`) AS `TIME`, a.title as TITLE, `text` AS `TEXT`, b.name AS CAT_NAME",
+            "APEXX_PREFIX_news AS a LEFT JOIN APEXX_PREFIX_news_category AS b ON (a.category = b.id)",
+                ( $this->param()->getIf("catid") ? "b.id = " . $this->param()->getInt("catid")." AND " : "" ). " " .
+                ($user->hasRight("news", "index", EXECUTION_TYPE::ADMIN) ? "" :
+                "  UNIX_TIMESTAMP(`time`) < ".time()." AND `time` IS NOT NULL AND ") . "1=1",
+            "`time` IS NULL  DESC, `time` DESC",
+            20, []
+        );
 
-            ORDER BY 
-                `time` DESC
-            ");
+        $this->assign("CURRENT_TIME", time());
 
-        $statement->execute();
-        $content = $statement->fetchAll();
+        $lists = $pager->prepare();
+        $pages = $pager->pages();
+        $this->assign("PAGES", $pages);
+        $this->assign("NEWS", $lists);
 
-        if ( $content )
+        if( $this->param()->getIf("catid") )
         {
-            $this->assign("CURRENT_TIME", time());
-            $this->assign("NEWS", $content);
-
-            if( $this->param()->getIf("catid") )
-            {
-                $catid = $this->param()->getInt("catid");
-                $stmt = $this->prepare("SELECT `name` AS `NAME` FROM APEXX_PREFIX_news_category WHERE id = :id");
-                $stmt->bindParam(":id", $catid);
-                $stmt->execute();
-                $this->assign("CAT_NAME", $stmt->fetch()["NAME"]);
-            }
-            else
-            {
-                $this->assign("CAT_NAME", NULL);
-            }
-
-            $this->render("index");
+            $catid = $this->param()->getInt("catid");
+            $stmt = $this->prepare("SELECT `name` AS `NAME` FROM APEXX_PREFIX_news_category WHERE id = :id");
+            $stmt->bindParam(":id", $catid);
+            $stmt->execute();
+            $this->assign("CAT_NAME", $stmt->fetch()["NAME"]);
         }
         else
         {
-            throw new \Exception("Can not show news. News unavailable.");
+            $this->assign("CAT_NAME", null);
         }
+
+        $this->render("index");
     }
 }
